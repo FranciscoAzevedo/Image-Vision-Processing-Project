1601c4e6e673155ab808c21bc63a572a9c2342dc
Francisco Azevedo <francisco.n.azevedo@gmail.com>
2017-11-10 16:16:12 +0000

    Upload do Relatório

---
+++ peopletracking.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    peopletracking.m                                       %
+%    Progama desenvolvido por: Francisco Azevedo (80966)    %
+%                              Luís Almeida ()              %
+%                              Francisco Pereira ()         %
+%    At IST, Lisbon 2017                                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function tracked_objs=peopletracking( file_names, depth_cam, rgb_cam, Rdtrgb,Tdtrgb)
+
+%Point cloud for camera 1
+
+%Point cloud for camera 2
+
+%Merging point clouds into world coordinate frame
+
+%Detecting background 
+
+%Detecting objects 
+
+%Making correspondence between current and last image
+
+%Drawing box around object and storing data
+
+end
