683d809e7071f2e91a56a489e5d894092f36944a
Francisco Azevedo <francisco.n.azevedo@gmail.com>
2017-12-05 19:48:50 +0000

    Turns out im shit

--- Proj_Part1/fg_detect.m
+++ Proj_Part1/fg_detect.m
@@ -5,16 +5,12 @@ clc;
 load('cameraparametersAsus.mat');
 
 % Opening dataset
-d1 = dir('rgb_image1_2.png');
-d_d1 = dir('depth1_2.mat');
-d2 = dir('rgb_image1_3.png');
-d_d2 = dir('depth1_3.mat');
+d1 = dir('rgb_image1_14.png');
+d_d1 = dir('depth1_14.mat');
 
 % Allocating memory
 ims1 = [];
 ims_d1 = [];
-ims2 = [];
-ims_d2 = [];
 
 % Converting image 1 to gray and to column vectors
 im = rgb2gray(imread(d1.name));
@@ -22,18 +18,9 @@ load(d_d1.name);
 ims1 = [ims1 im(:)];
 ims_d1 =[ims_d1 depth_array(:)];
 
-% Converting image 2 to gray and to column vectors
-im2 = rgb2gray(imread(d2.name));
-load(d_d2.name);
-ims2 = [ims2 im2(:)];
-ims_d2 =[ims_d2 depth_array(:)];
-
 % Apply filter to have stronger border contrast
 median_im1 = median(double(ims1),2);
 median_depth1 = median(double(ims_d1),2);
-
-median_im2 = median(double(ims2),2);
-median_depth2 = median(double(ims_d2),2);
 
 % Apply interpolation to fix some background
 
@@ -42,31 +29,29 @@ median_depth2 = median(double(ims_d2),2);
 bg_img1 = reshape(median_im1,[480 640]); %RGB BG can be usefull but prob not
 bg_depth1 = reshape(median_depth1,[480 640]);
 
-bg_img2 = reshape(median_im2,[480 640]); %RGB BG can be usefull but prob not
-bg_depth2 = reshape(median_depth2,[480 640]);
-
 % Getting the foreground in depth between 2 images (store value and bin matrix)
-fg_depth = abs(bg_depth2 - bg_depth1);
-fg_depth_bin = abs(bg_depth2 - bg_depth1) > 200; 
-fg_depth(fg_depth < 200) = 0; % Only store above 200
+load(d1.name);
+fg_depth = abs(double(depth_array) - bg_depth1);
+fg_depth_bin = fg_depth > 50; %Save binary img
+fg_filtered = bwareaopen(fg_depth_bin,50);
 
-fg_img = abs(bg_img2 - bg_img1); 
-fg_img(fg_img < 50) = 0; %Only store above 50
+fg_final = fg_depth.*fg_filtered;
 
 %Plots
 figure(1);
-imshow(fg_depth);
+imshow([im 255*fg_depth])
 figure(2);
-imshow(fg_img);
+imshow([im 255*fg_filtered]);
 
 %end
 
+% 3 LABELING NOT WORKING
 xyz_1 = get_xyz_asus(fg_depth(:),[480 640],1:640*480, cam_params.Kdepth ,1,0);
 
-CC = bwconncomp(xyz_1)
+pc1 = pointCloud(xyz_1);
+figure(3);
+showPointCloud(pc1);
 
-
-
-
+CC = bwconncomp(xyz_1, 18);
 
 
--- Proj_Part1/track3D_part1.m
+++ Proj_Part1/track3D_part1.m
@@ -10,13 +10,11 @@ function objects = track3D_part1(imgseq1, imgseq2, cam_params, cam1toW, cam2toW)
 
 %Get Foreground in depth points for both cameras
 
-[foreground_depth1, foreground_img1] = fg_detect( RGB_PATH1, RGB_PATH2, DEPTH_PATH1, DEPTH_PATH2);
-[foreground_depth2, foreground_img2] = fg_detect(RGB_PATH1, RGB_PATH2, DEPTH_PATH1, DEPTH_PATH2);
-
-%Correspond those points into 3D
-
+[fg_depth1,fg_depth1_bin, fg_img1, fg_img1_bin] = fg_detect( RGB_PATH1, RGB_PATH2, DEPTH_PATH1, DEPTH_PATH2);
+[fg_depth2,fg_depth2_bin, fg_img2, fg_img2_bin] = fg_detect(RGB_PATH1, RGB_PATH2, DEPTH_PATH1, DEPTH_PATH2);
 
 %Draw a 2D box around it
+
 
 %Draw 3D around it and fix having it projected into the BG
 
