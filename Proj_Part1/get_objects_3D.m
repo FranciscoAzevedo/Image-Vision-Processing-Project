7b6a941ac37dc3212dbd1870484c3fbd5b52eb9f
Unknown <luis.mig.almeida96@gmail.com>
2017-12-19 20:24:38 +0000

    Almost Part1 without tracking

--- Proj_Part1/get_objects_3D.m
+++ Proj_Part1/get_objects_3D.m
@@ -1,14 +1,35 @@
-% function [ obj_cam ] = get_objects_3D(labels, fg_depth, xyz_cam)
+function [ obj_cam ] = get_objects_3D(labels, fg_depth, xyz_cam)
+    
     label_num = 1;
     obj_cam = [];
-    
     % Find the rows and columns of pixels of a specific object
     [r,c] = find(labels == label_num);
+    outliers = zeros(size(depth1));
     
     while( isempty(r) == 0 || isempty(c) == 0)
+        
+        % Detect outliers that are far from the mean depth
+        soma = 0;
+        for i = 1:length(r)
+           soma = soma + double(depth1(r(i),c(i)));
+        end
+        avg_depth = soma/length(r);
+        
+        for i = 1:length(r)
+            if(abs(double((depth1(r(i),c(i))) - avg_depth)) > 200)
+                labels(r(i),c(i)) = 0;
+            end
+        end
+        
+        % Update to discard outliers
+        [r,c] = find(labels == label_num);
     
         % Get the 3D points corresponding to "r" and "c"
-        obj_cam(label_num).xyz = xyzd1(sub2ind([480 640],round(r),round(c)),:);
+        obj_cam(label_num).xyz = xyzd1(sub2ind([480 640],r,c),:);
+        
+        % Delete invalid point
+        todelete = (obj_cam(label_num).xyz(:,1) == 0 & obj_cam(label_num).xyz(:,2) == 0 & obj_cam(label_num).xyz(:,3) == 0);
+        obj_cam(label_num).xyz(todelete,:) = [];
         
         % Get the centroid of these points
         [idx,C] = kmeans(obj_cam(label_num).xyz,1);
@@ -19,16 +40,6 @@
         % Find the rows and columns of pixels of a specific object
         [r,c] = find(labels == label_num);
         
-    end
-    
-%     pc1 = pointCloud(xyzd1);
-%     showPointCloud(pc1);
-%     hold on;
-    
-    pc2 = pointCloud(obj_cam(1).xyz);
-    showPointCloud(pc2);
-    hold on;
-    
-    scatter3(obj_cam(1).centroid(1),obj_cam(1).centroid(2),obj_cam(1).centroid(3),'*r');
-% end
+end
+
 
--- Proj_Part1/track3D_part2.m
+++ Proj_Part1/track3D_part2.m
@@ -6,36 +6,34 @@
 %    At IST, Lisbon 2017                                    %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function objects = track3D_part2(imgseq1, imgseq2, cam_params, cam1toW, cam2toW)
-%% Get Background for the whole program
-imgseq1 = dir('*.png');
-imgseq2 = dir('*.png');
-dd = dir('*.mat');
-ims = [];
-imsd = [];
+function objects = track3D_part2(imgseq1, imgseq2, cam_params)
+    
+    imgseq1 = dir('*.png');
+    imgseq2 = dir('*.png');
+    load(cam_params);
 
-% Run RANSAC, SIFT and Procrustes to get R and T world
-[R,T,P1,P2] = get_T_R_world( im1, imd1, im2, imd2 )
+    [depth1,depth2,im1,im2,xyzd1,xyzd2,imd1,imd2] = setup_img('depth1_15.mat','depth2_15.mat','rgb_image1_15.png','rgb_image2_15.png');
 
-% Get foreground for both cameras
-[fg_depth1,fg_depth1_bin, fg_img1, fg_img1_bin] = fg_detect(RGB_PATH1, DEPTH_PATH1, bg_depth1 );
-[fg_depth2,fg_depth2_bin, fg_img2, fg_img2_bin] = fg_detect(RGB_PATH2, DEPTH_PATH2, bg_depth2);
+    % Run RANSAC, SIFT and Procrustes to get R and T world
+    [R,T,P1,P2] = get_T_R_world( im1, imd1, im2, imd2 )
 
-% Detect Objects in 3D
-labels_1 = bwlabel(fg_depth1_bin);
-labels_2 = bwlabel(fg_depth2_bin);
+    % Get foreground for both cameras
+    [fg_depth1,fg_depth1_bin, fg_img1, fg_img1_bin] = bg_fg_detect(RGB_PATH1, DEPTH_PATH1, bg_depth1 );
+    [fg_depth2,fg_depth2_bin, fg_img2, fg_img2_bin] = fg_detect(RGB_PATH2, DEPTH_PATH2, bg_depth2);
 
-[obj_cam1]= get_objects_3D(labels_1,fg_depth1, xyz1);
-[obj_cam2]= get_objects_3D(labels_2,fg_depth2, xyz2);
-
-
-transform_Cam2to_cam1
-
-
-% Draw 3D around the objects
+    % Label objects in 2D
+    labels = bwlabel(fg_depth1_bin);
+    labels_2 = bwlabel(fg_depth2_bin);
+    
+    % Get PC and centroids for each object in each camera
+    [obj_cam1]= get_objects_3D(labels_1,fg_depth1, xyz1);
+    [obj_cam2]= get_objects_3D(labels_2,fg_depth2, xyz2);
+    
+    % Get the final cornerpoints for all objects
+    [objects] = object_consensus(obj_cam1, obj_cam2, R, T);
 
 
-%Using SIFT to get correspondence between points in RGB
-%Making correspondence between current and last image
+    %Using SIFT to get correspondence between points in RGB
+    %Making correspondence between current and last image
 
 end
