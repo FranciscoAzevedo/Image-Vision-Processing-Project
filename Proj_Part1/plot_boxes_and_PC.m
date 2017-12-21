f570d5d3757dd14aea624ab64017e1cb3d13e420
Unknown <luis.mig.almeida96@gmail.com>
2017-12-21 23:58:16 +0000

    Caixa a volta do prof
    Caixa a volta do prof

--- Proj_Part1/plot_boxes_and_PC.m
+++ Proj_Part1/plot_boxes_and_PC.m
@@ -1,15 +1,24 @@
-function [] = plot_boxes_and_PC(k,xyz1,xyz2,objects,R,T)
+function [] = plot_boxes_and_PC(xyz1,xyz2,k, objects,R,T)
     
     xyz_2_to_1 = (xyz2*R) + ones(length(xyz2),1)*(T(1,:));
     
-    figure(1);    
-    pc1 = pointCloud(xyzd1);
-    showPointCloud(pc1);
+    figure();    
+    pc1 = pointCloud(xyz1);
+    pc2 = pointCloud(xyz_2_to_1);
+    PC = pcmerge(pc1,pc2,0.001);
+    showPointCloud(PC);
     title('CAM 1');
-    
-    for i = length(objects)
-        figure(i);
-        plotminbox(objects.(i))
-        hold on;
+    if (isempty(objects)==0)
+        for i = length( objects)
+        
+            if (objects(i).frames_tracked(length(objects(i).frames_tracked))==k)
+            hold on;
+            cornerpoints(:,1) =objects(i).X(length(objects(i).frames_tracked),:)';
+            cornerpoints(:,2) =objects(i).Y(length(objects(i).frames_tracked),:)';
+            cornerpoints(:,3) =objects(i).Z(length(objects(i).frames_tracked),:)';
+            plotminbox( cornerpoints );
+            end
+
+        end
     end
 end
